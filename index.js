019635297490920b5b5d53bb3f8ff7dfa889fa5a
Thomas Coradetti <tmcoradetti@gmail.com>
2020-07-22 19:03:04 -0400

    added array of prompt questions

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter the title of your project:"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter any installation instructions for your project:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter the usage information for your project:"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please enter the contribution guidelines for your project:"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter the test instructions for your project:"
+    }
 ];
 
 // function to write README file
