8e00fc928ca6e519188ca96a6e05bda2612247a5
Thomas Coradetti <tmcoradetti@gmail.com>
2020-07-22 19:32:05 -0400

    added the rest of the prompts

--- index.js
+++ index.js
@@ -33,17 +33,38 @@ const questions = [
         type: "input",
         name: "test",
         message: "Please enter the test instructions for your project:"
+    },
+    {
+        type: "checkbox",
+        name: "licenses",
+        message: "Please select any licenses for your project:",
+        choices: [
+            {name: "Apache 2"},
+            {name: "MIT"},
+            {name: "GPL"}
+        ]
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address:"
     }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+};
 
 // function to initialize program
 function init() {
+    return inquirer.prompt(questions);
 
-}
+};
 
 // function call to initialize program
 init();
